e6670284f7e984b1a1cdf3a05334f0a2d78c98e6
Cristian Vianey Cáceres Labrador <80643642+cristiancacereslabrador@users.noreply.github.com>
2024-04-26 11:56:35 -0500

    imBackAbr2024

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,84 +1,113 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const server = require('../server');
+const server = require("../server");
 
-const chaiHttp = require('chai-http');
+const chaiHttp = require("chai-http");
 chai.use(chaiHttp);
 
-suite('Functional Tests', function () {
+// Suite de pruebas de integración con chai-http
+suite("Functional Tests", function () {
   this.timeout(5000);
-  suite('Integration tests with chai-http', function () {
-    // #1
-    test('Test GET /hello with no name', function (done) {
+
+  suite("Integration tests with chai-http", function () {
+    // Prueba #1
+    test("Test GET /hello with no name", function (done) {
       chai
         .request(server)
         .keepOpen()
-        .get('/hello')
+        .get("/hello")
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello Guest');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, "hello Guest");
           done();
         });
     });
-    // #2
-    test('Test GET /hello with your name', function (done) {
+
+    // Prueba #2
+    test("Test GET /hello with your name", function (done) {
       chai
         .request(server)
         .keepOpen()
-        .get('/hello?name=xy_z')
+        .get("/hello?name=CristianVianey")
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello xy_z');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, "hello CristianVianey");
           done();
         });
     });
-    // #3
+
+    // Prueba #3
     test('Send {surname: "Colombo"}', function (done) {
       chai
         .request(server)
         .keepOpen()
-        .put('/travellers')
-
+        .put("/travellers")
+        .send({ surname: "Colombo" })
         .end(function (err, res) {
-          assert.fail();
-
+          assert.equal(res.status, 200);
+          assert.equal(res.type, "application/json");
+          assert.equal(res.body.name, "Cristoforo");
+          assert.equal(res.body.surname, "Colombo");
           done();
         });
     });
-    // #4
-    test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
 
-      done();
+    // Prueba #4
+    test('Send {surname: "da Verrazzano"}', function (done) {
+      chai
+        .request(server)
+        .keepOpen()
+        .put("/travellers")
+        .send({ surname: "da Verrazzano" })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, "application/json");
+          assert.equal(res.body.name, "Giovanni");
+          assert.equal(res.body.surname, "da Verrazzano");
+          done();
+        });
     });
   });
 });
 
-const Browser = require('zombie');
+const Browser = require("zombie");
+// Configuración del sitio para Zombie.js
+Browser.site = "http://127.0.0.1:3000";
+// Browser.site =
+//   "https://redesigned-space-invention-xg6xgq7xgqfvr-3000.app.github.dev/";
+// Suite de pruebas funcionales con Zombie.js
+suite("Functional Tests with Zombie.js", function () {
+  const browser = new Browser();
 
-suite('Functional Tests with Zombie.js', function () {
-  this.timeout(5000);
+  // Configuración de la visita a la página antes de las pruebas
+  suiteSetup(function (done) {
+    return browser.visit("/", done);
+  });
 
-
-
-  suite('Headless browser', function () {
-    test('should have a working "site" property', function() {
-      assert.isNotNull(browser.site);
+  // Prueba #5
+  test('submit "surname" : "Colombo" - write your e2e test...', function (done) {
+    // Llenar el formulario y enviarlo
+    browser.fill("surname", "Colombo").pressButton("submit", function () {
+      // Asegurar que la prueba es exitosa
+      browser.assert.success();
+      browser.assert.text("span#name", "Cristoforo");
+      browser.assert.text("span#surname", "Colombo");
+      browser.assert.element("span#dates", 1);
+      done(); // Es una prueba asíncrona, así que llamamos a 'done()'
     });
   });
 
-  suite('"Famous Italian Explorers" form', function () {
-    // #5
-    test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
-    });
-    // #6
-    test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
-
+  // Prueba #6 (actualmente falla porque tiene assert.fail())
+  test('submit "surname" : "Vespucci" - write your e2e test...', function (done) {
+    // Comentar o eliminar la línea assert.fail() para habilitar la prueba
+    // assert.fail();
+    browser.fill("surname", "Vespucci").pressButton("submit", function () {
+      // Asegurar que la prueba es exitosa
+      browser.assert.success();
+      browser.assert.text("span#name", "Amerigo");
+      browser.assert.text("span#surname", "Vespucci");
+      browser.assert.element("span#dates", 1);
       done();
     });
   });
